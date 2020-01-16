5ad537ee710300ce21f8c4ca8ac9ac0282999ef4
Chris Fauerbach <chrisfauerbach@gmail.com>
2020-01-16 14:32:15 -0500

    Adding week one

---
+++ week1/HelloWorld.cpp
@@ -0,0 +1,12 @@
+// HelloWorld.cpp :
+/* 
+   Defines the entry point for 
+   the console application.
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Hello World" << endl;
+	return 0;
+}
